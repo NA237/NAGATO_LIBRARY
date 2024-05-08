853fa6dc91c6b74178197ab713bed0957c0a5e26
NANA TONGWA <99105141+NA237@users.noreply.github.com>
2024-05-08 23:39:42 +0200

    NAGATO_LIBRARY
    Welcome to the Digital Library! Explore our collection of digital books, historical documents and classic literary works. Immerse yourself in a world of knowledge and entertainment, accessible wherever you are.

---
+++ BOOKS.js
@@ -0,0 +1,15 @@
+new Swiper(
+    '.swiper',
+    {
+        effect: 'cards',
+        grabCursor:true,
+        initialSlide: 2,
+        speed:500,
+        loop:true,
+        rotate:true,
+        mousewheel: {
+            invert : false
+        },
+
+    },
+);
